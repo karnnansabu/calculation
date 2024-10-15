536976b08d22199ad3982fb22d911bf7d94018e0
karnnansabu <karnnansabu@gmail.com>
2024-10-15 12:50:25 +0530

    exit

--- add.py
+++ add.py
@@ -11,10 +11,3 @@ num2 = float(input("Enter the second number: "))
 # Call the function and display the result
 result = add_numbers(num1, num2)
 print(f"The sum of {num1} and {num2} is {result}")
-
-
-print("Hello world")
-
-print("I am sonu")
-
-print("am 26 years old")
